932f99955195d348178bf7d5bd49067522a6e272
vinaymopidevi <vinay.mopidevi@gmail.com>
2017-06-19 01:30:41 +0530

    first commit

---
+++ src/main/java/com/ual/auto/SeleniumSample/CopyOfApp.java
@@ -0,0 +1,13 @@
+package com.ual.auto.SeleniumSample;
+
+/**
+ * Hello world!
+ *
+ */
+public class CopyOfApp 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
